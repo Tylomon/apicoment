8c4d25a6ea5ff91245f3e08cea72bd235203a350
Tylomon <tylomn@gmail.com>
2021-08-04 18:19:41 -0500

    Api: Extraccion de Comentarios

--- api.py
+++ api.py
@@ -1,8 +1,37 @@
 from fastapi import FastAPI
+from google_play_scraper import app
 
+#Funciones Operativas
 
-app=FastAPI()
+def extraccion():
+    result = app(
+    'com.clarocolombia.miclaro',
+    lang='es', 
+    country='co')
+    comentarios=result['comments']
+    return comentarios
+
+def dictionario():
+    receptor=extraccion()
+    comentarios_dict=dict(enumerate(set(receptor)))
+    return comentarios_dict 
+
+perri=[1,2]
+
+appli=FastAPI()
 #coment
-@app.get('/')
+@appli.get('/')
 def rea_root():
-    return {"Welcome":"wenas"}
+    return {"Bienvenido":"API extraccion y calificacion de Comentarios para la aplicacion mi claro"}
+
+@appli.get('/comments')
+def visualize_comments():   
+    return dictionario()
+
+@appli.post('/comments')
+def visualize_Ncomments(n):
+    number=int(n)
+    print(type(number))
+    receptor=extraccion()
+    comentarios_dict=dict(enumerate(set(receptor[0:number])))  
+    return comentarios_dict
